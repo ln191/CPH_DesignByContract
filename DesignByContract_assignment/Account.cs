e9ab0ce44ee3d8d1c573fb3c9472fa4a6837ff69
ln191 <cph-ln191@cphbusiness.dk>
2016-12-04 16:49:19 +0100

    made program and account class with functions, but without contract check

---
+++ DesignByContract_assignment/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignByContract_assignment
+{
+    public class Account
+    {
+        private double balance = 1000.00;
+        public double Balance { get; }
+
+        public Account()
+        {
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Error:  you cannot deposit a amount that is 0 or less");
+            }
+            else
+            {
+                balance += amount;
+                Console.WriteLine("You have deposit: {0}", amount);
+                Console.WriteLine("Your new balance: {0}", balance);
+                //return balance;
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Error:  you cannot Withdraw an amount that is 0 or less");
+            }
+            else if (amount > balance)
+            {
+                Console.WriteLine("Error: you cannot withdraw an amount greater than your balance");
+                Console.WriteLine("Your current balance: {0}", balance);
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine("You have withdrawn: {0}", amount);
+                Console.WriteLine("Your new balance: {0}", balance);
+            }
+        }
+    }
+}
--- DesignByContract_assignment/Program.cs
+++ DesignByContract_assignment/Program.cs
@@ -6,10 +6,61 @@ using System.Threading.Tasks;
 
 namespace DesignByContract_assignment
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.Title = "Your account";
+            Account account = new Account();
+            bool running = true;
+            Console.WriteLine("To deposit a amount press d and then enter the amount and press Enter");
+            Console.WriteLine("To withdraw an amount press w and then enter the amount and press Enter");
+            Console.WriteLine("To Exit program press e");
+            Console.WriteLine("Your current balance is: {0}", account.Balance);
+            while (running)
+            {
+                Console.WriteLine("Ready for Command:");
+                ConsoleKey key = Console.ReadKey().Key;
+                if (key == ConsoleKey.D)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("How must would you like to deposit:");
+                    string input = Console.ReadLine();
+                    double value;
+                    if (double.TryParse(input, out value))
+                    {
+                        account.Deposit(value);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error you input is not valet, your amount must consist of numbers");
+                    }
+                }
+                else if (key == ConsoleKey.W)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("How must would you like to withdraw:");
+                    string input = Console.ReadLine();
+                    double value;
+                    if (double.TryParse(input, out value))
+                    {
+                        account.Withdraw(value);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error you input is not valet, your amount must consist of numbers");
+                    }
+                }
+                else if (key == ConsoleKey.E)
+                {
+                    running = false;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Error: not a valet input");
+                }
+            }
         }
     }
 }
